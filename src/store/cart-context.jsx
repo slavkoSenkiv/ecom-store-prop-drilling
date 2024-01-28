7413dd62c59cbb0ada599f26be83880523f9785b
Yaroslav Senkiv <slavko.senkiv@gmail.com>
2024-01-28 09:04:18 +0100

    finish add item logic

--- src/store/cart-context.jsx
+++ src/store/cart-context.jsx
@@ -9,24 +9,34 @@ export const CartContext = createContext({
 
 function cartReducer(state, action) {
   if (action.type === "ADD_ITEM") {
-
     const updatedCartItems = [...state.items];
-
-    const itemToAddIndex = DUMMY_PRODUCTS.findIndex(
+    const existingItemInCartIndex = updatedCartItems.findIndex(
       (product) => product.id === action.payload
     );
+    const existingInCartItem = updatedCartItems[existingItemInCartIndex];
 
-    if (existingCartItem) {
-
+    if (existingInCartItem) {
+      const updatedItem = {
+        ...existingInCartItem,
+        quantity: existingInCartItem.quantity + 1,
+      };
+      updatedCartItems[existingInCartItem] = updatedItem;
     } else {
-
-      updatedCartItems.push(DUMMY_PRODUCTS[itemToAddIndex]);
-
+      const product = DUMMY_PRODUCTS.find(
+        (product) => product.id === action.payload
+      );
+      updatedCartItems.push({
+        id: product.id,
+        name: product.title,
+        price: product.price,
+        quantity: 1,
+      });
     }
 
-    return { 
-      ...state, 
-      items: updatedCartItems };
+    return {
+      ...state,
+      items: updatedCartItems,
+    };
   }
 
   if (action.type === "UPDATE_ITEM") {
@@ -58,6 +68,6 @@ export default function CartContextProvider({ children }) {
   };
 
   return (
-    <CartContext.Provider value={ctxValue}> {children}</CartContext.Provider>
+    <CartContext.Provider value={ctxValue}>{children}</CartContext.Provider>
   );
 }
